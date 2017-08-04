fb3b721da62e8ea44767a7d162a0307b6bae48e9
Kirill Danshin <k@guava.by>
2017-08-04 23:56:55 +0300

    reducers/product: add data
    Signed-off-by: Kirill Danshin <k@guava.by>

--- src/reducers/product.js
+++ src/reducers/product.js
@@ -3,6 +3,10 @@ import { Map } from 'immutable'
 const cdnAddr = 'https://scnt-test-cdn.danshin.pro/'
 
 const getLink = path => `${cdnAddr}${path}`
+const getRateIconLink = name => getLink(`rates/${name}.svg`)
+
+// NOTE: placeholder data here.
+// should not be used as main storage.
 
 export const initialState = Map({
 	heading: 'Cartier',
@@ -26,5 +30,37 @@ export const initialState = Map({
 		},
 	],
 	notes: [1, 2, 3, 4, 5, 6].map(v => getLink(`notes/notes-${v}.png`)),
+	rates: [
+		{
+			name: 'Scent Type',
+			choise: 'Spicy',
+			iconLink: getRateIconLink('spicy'),
+			votes: 35,
+		},
+		{
+			name: 'Personality',
+			choise: 'Summer',
+			iconLink: getRateIconLink('summer'),
+			votes: 25,
+		},
+		{
+			name: 'Occasion',
+			choise: 'Date Night',
+			iconLink: getRateIconLink('date-night'),
+			votes: 36,
+		},
+		{
+			name: 'Season',
+			choise: 'Office',
+			iconLink: getRateIconLink('office'),
+			votes: 35,
+		},
+		{
+			name: 'Attributes',
+			choise: 'Office',
+			iconLink: getRateIconLink('office'),
+			votes: 17,
+		},
+	],
 	selectedMediaCount: 0,
 })
